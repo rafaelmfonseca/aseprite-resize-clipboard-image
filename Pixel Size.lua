b38121dac8f13b5c2d297e296f2a8b11bf978de3
Rafael Moreira Fonseca <rarafonseca@hotmail.com>
2022-02-26 17:37:22 -0300

    feat: open new file then zooms

--- Pixel Size.lua
+++ Pixel Size.lua
@@ -1,50 +1,22 @@
 function init(plugin)
 
     function ResizePixel()
-        local dlg = Dialog {
-            title = "Pixel Size"
+        app.command.NewFile {
+            ui = true,
+            colorMode = ColorMode.RGB,
+            fromClipboard = true
         }
-        
-        dlg:button {
-            id = "cancel",
-            text = "Cancel"
-        }
-        dlg:button {
-            id = "ok",
-            text = "OK",
-            focus = true,
-            onclick = function()
-                local args = dlg.data
 
-                if args.ok then
-                    app.command.NewFile {
-                        ui = true,
-                        colorMode = ColorMode.RGB,
-                        fromClipboard = true
-                    }
+        local sprite = app.activeSprite
 
-                    local sprite = app.activeSprite
+        if sprite then
+            app.command.ShowPixelGrid { }
+            app.command.Zoom { percentage = 800, action = "in", focus = "center" }
+        else
+            app.alert("There is no valid image on clipboard.")
+        end
 
-                    if sprite then
-                        -- sprite.layers[1]:cel(1).image:drawPixel(0, 0, Color(199,90,104));
-                        -- local sprite = Sprite(10, 10)
-
-                        app.command.GridSettings {
-                            ui = true,
-                            width = 1,
-                            height = 1,
-                            gridBounds = Rectangle(0, 0, 1, 1)
-                        }
-                    else
-                        app.alert("There is no valid image on clipboard.")
-                    end
-
-                    app.refresh()
-                    dlg:close()
-                end
-            end
-        }
-        dlg:show()
+        app.refresh()
     end
 
     plugin:newCommand {
