860cc10f63df23370bd73b707e3f3f566a6ea9a0
Rafael Moreira Fonseca <rarafonseca@hotmail.com>
2022-02-26 17:45:27 -0300

    feat: dialog with properties

--- Pixel Size.lua
+++ Pixel Size.lua
@@ -1,6 +1,10 @@
 function init(plugin)
 
-    function ResizePixel()
+    local function ResizeImage(data)
+        -- app.alert("Pixel size: " .. data.size)
+    end
+
+    local function OpenClipboardImage()
         app.command.NewFile {
             ui = true,
             colorMode = ColorMode.RGB,
@@ -12,6 +16,19 @@ function init(plugin)
         if sprite then
             app.command.ShowPixelGrid { }
             app.command.Zoom { percentage = 800, action = "in", focus = "center" }
+
+            local dlg = Dialog { title = "Pixel Size" }
+            dlg:separator { text = "Pixel properties" }
+            dlg:entry { id = "size", label = "Size" }
+            dlg:newrow()
+            dlg:button { id = "cancel", text = "Cancel" }
+            dlg:button { id = "ok", text = "OK", focus = true,
+                onclick = function()
+                    ResizeImage(dlg.data);
+                    dlg:close();
+                end
+            }
+            dlg:show()
         else
             app.alert("There is no valid image on clipboard.")
         end
@@ -19,12 +36,9 @@ function init(plugin)
         app.refresh()
     end
 
-    plugin:newCommand {
-        id = "PixelSize",
-        title = "Pixel Size",
-        group = "sprite_size",
+    plugin:newCommand { id = "PixelSize", title = "Pixel Size", group = "sprite_size",
         onclick = function()
-            ResizePixel()
+            OpenClipboardImage()
         end
     }
 
